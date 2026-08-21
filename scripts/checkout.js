87bc54254fb1f6bd72b22b4d629b49c14b6c3ac3
Satti Keerthi Ratna Kousalya Devi <keerthiratna.satti@gmail.com>
2026-08-21 12:21:18 +0530

    convert code to promises in checkout

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,8 +2,28 @@ import {renderOrderSummary} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import "../data/cart-class.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
+
+Promise.all([
+    new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve();
+        })
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    })
+]).then(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
+
+/*
 loadProducts(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 })
+*/
